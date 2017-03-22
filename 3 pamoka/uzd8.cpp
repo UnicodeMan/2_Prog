4830334a26ba1041c7040ff8dd977d3fb4bd8528
UnicodeMan <lveitas956@gmail.com>
2017-03-22 20:25:26 +0200

    Update uzd8.cpp

--- 3 pamoka/uzd8.cpp
+++ 3 pamoka/uzd8.cpp
@@ -2,12 +2,35 @@
 using namespace std;
 main()
 {
-setlocale(LC_CTYPE, "");
+setlocale(LC_CTYPE, ""); // Lietuvinam
+/*
+  Petras išėjo iš namų, kai laikrodis rodė v1 valandų ir m1 minučių. Į gimnaziją Petro kelionė
+  trunka m2 minučių. Parašykite programą, kuri ekrane parodytų pranešimą apie tai, ar
+  Petras nepavėluos į pamoką, prasidedančią v valandų ir m minučių.
+*/
 
+int v1,m1,m2,v,m,mm1,mm;
 
+cout << "Petras iš namų išėjo kai laikrodis rodė..." << endl << "Valandų? ";
+cin  >> v1;
+cout << "Minučių? ";
+cin  >> m1;
+cout << "Kiek minučių Petras truko nueiti iki gimnazijos? ";
+cin  >> m2;
+cout << "Pamokos gimnazijoje prasidėjo kai laikrodis rodė..." << endl << "Valandų? ";
+cin  >> v;
+cout << "Minučių? ";
+cin  >> m;
 
+// smulkinimas
+mm  = v  * 60 + m;
+mm1 = v1 * 60 + m1;
 
-//cout << "Hello World!ę";
-//return 0;
-
+if (mm1 + m2 < mm) { // Išėjo iš namų + kiek trunka < kada prasideda
+    cout << "Petras į pamoką nepavėluos.";
 }
+else {
+    cout << "Petras į pamoką pavėluos.";
+}
+}
+
