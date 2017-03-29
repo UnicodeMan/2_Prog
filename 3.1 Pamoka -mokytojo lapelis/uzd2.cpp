461ea6661da1689177712c86508649b2d834c881
UnicodeMan <lveitas956@gmail.com>
2017-03-29 14:21:07 +0300

    Create uzd2.cpp

---
+++ 3.1 Pamoka -mokytojo lapelis/uzd2.cpp
@@ -0,0 +1,49 @@
+// Iš šešiaženklio skaičiaus išbraukti visus trejetus ir penketus.
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    setlocale(LC_ALL, "Lithuanian");
+    int sk, s1, s2, s3, s4, s5, s6, kiekis;
+    cout << "Įveskite šešiaženklį skaičių: ";
+    cin >> sk;
+
+    s1 = sk / 100000 % 10;
+    s2 = sk /  10000 % 10;
+    s3 = sk /   1000 % 10;
+    s4 = sk /    100 % 10;
+    s5 = sk /     10 % 10;
+    s6 = sk          % 10;
+
+    if (s1 == 5 || s1 == 3) {}
+    else {
+        cout << s1;
+    }
+    if (s2 == 5 || s2 == 3) {}
+    else {
+        cout << s2;
+    }
+    if (s3 == 5 || s3 == 3) {}
+    else {
+        cout << s3;
+    }
+    if (s4 == 5 || s4 == 3) {}
+    else {
+        cout << s4;
+    }
+    if (s5 == 5 || s5 == 3) {}
+    else {
+        cout << s5;
+    }
+    if (s6 == 5 || s6 == 3) {}
+    else {
+        cout << s6;
+    }
+
+
+
+	//cout << "labas" << endl;
+	//return 0;
+
+}
