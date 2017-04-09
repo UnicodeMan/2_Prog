f61440af8d753f9661a70a0d31ffb0b5cb972480
UnicodeMan <lveitas956@gmail.com>
2017-04-09 22:08:50 +0300

    Create uzd4.cpp
    užduotis nepradėta

---
+++ 4 Pamoka/uzd4.cpp
@@ -0,0 +1,22 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 4 UŽDUOTIS:                                                                                                   //
+// Elektros grandinę sudaro n lygiagrečiai sujungtų laidininkų, kurių varžos yra R1,Rr2.Rr3, ....,Rn omų.        //
+// Parašykite programą, kuri apskaičiuotų grandinės varžą R.                                                     //
+// --------------------------------------------------------------------------------------------------------------//
+// Lygiagrečiai sujungiamų laidininkų varžos apskaičiavimas:                                                     //
+// n = 3, t.y kai laidininkų trys: R1, R2, R3, tai R = 1 / R1 + 1 / R2 + 1 / R3                                  //
+// --------------------------------------------------------------------------------------------------------------//
+// **** Užduotis nepradėta! *****                                                                                //
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream.h>
+using namespace std;
+setlocale(LC_CTYPE, "");
+main()
+
+{
+
+
+
+
+}
