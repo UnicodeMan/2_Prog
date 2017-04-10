f4d4a0c01832a0de0511b9b9108bf55893c60f31
UnicodeMan <lveitas956@gmail.com>
2017-04-10 14:28:13 +0300

    Update uzd2.cpp

--- 3.1 Pamoka -mokytojo lapelis/uzd2.cpp
+++ 3.1 Pamoka -mokytojo lapelis/uzd2.cpp
@@ -1,44 +1,42 @@
 // Iš šešiaženklio skaičiaus išbraukti visus trejetus ir penketus.
-// neatitinka reikalavimų:
-// skaičius be trejetų ir penketų turi būti viename kintamajame!
+// //neatitinka reikalavimų:
+// //skaičius be trejetų ir penketų turi būti viename kintamajame!
 #include <iostream>
 using namespace std;
 int main ()
 {
-    setlocale(LC_ALL, "Lithuanian");
-    int sk, s1, s2, s3, s4, s5, s6, kiekis;
-    cout << "Įveskite šešiaženklį skaičių: ";
+    setlocale(LC_CTYPE, "");
+    int sk, s1, s2, s3, s4, s5, s6,sk2 = 0;
+    cout << "Iveskite sesiazenkli skaiciu: ";
     cin >> sk;
 
-    s1 = sk / 100000 % 10;
+    s1 = sk / 100000; //% 10; //nebūtina
     s2 = sk /  10000 % 10;
     s3 = sk /   1000 % 10;
     s4 = sk /    100 % 10;
     s5 = sk /     10 % 10;
     s6 = sk          % 10;
 
-    if (s1 == 5 || s1 == 3) {}
-    else {
-        cout << s1;
+
+
+    if (s1 != 5 && s1 != 3) {
+        sk2 = (sk2 + s1) * 10;
     }
-    if (s2 == 5 || s2 == 3) {}
-    else {
-        cout << s2;
+    if (s2 != 5 && s2 != 3) {
+        sk2 = (sk2 + s2) * 10;
     }
-    if (s3 == 5 || s3 == 3) {}
-    else {
-        cout << s3;
+    if (s3 != 5 && s3 != 3) {
+        sk2 = (sk2 + s3) * 10;
     }
-    if (s4 == 5 || s4 == 3) {}
-    else {
-        cout << s4;
+    if (s4 != 5 && s4 != 3) {
+        sk2 = (sk2 + s4) * 10;
     }
-    if (s5 == 5 || s5 == 3) {}
-    else {
-        cout << s5;
+    if (s5 != 5 && s5 != 3) {
+        sk2 = (sk2 + s5) * 10;
     }
-    if (s6 == 5 || s6 == 3) {}
-    else {
-        cout << s6;
+    if (s6 != 5 && s6 != 3) {
+        sk2 = (sk2 + s6) * 10;
     }
+    sk2 = sk2 / 10;
+    cout << "Naujas skaicius: " << sk2;
 }
