982e0c6da0c0028347b48968d361c27c4c7c027f
UnicodeMan <lveitas956@gmail.com>
2017-04-10 14:29:47 +0300

    Update uzd3.cpp

--- 3.1 Pamoka -mokytojo lapelis/uzd3.cpp
+++ 3.1 Pamoka -mokytojo lapelis/uzd3.cpp
@@ -1,12 +1,13 @@
 /*
 	Besilaikanti dietos moksleivė svarstyklėmis pasvėrė šešis
-	pyragaičius. Tuos, kurie buvo lengvesni negu 50g, ji suvalgė.
+	pyragaičius. Tuos, kurie buvo lengvesni negu 50g, ji suvalgė. svorį
 */
 #include <iostream>
 using namespace std;
 int main ()
 {
-    int p1,p2,p3,p4,p5,p6,is_viso;
+    int p1,p2,p3,p4,p5,p6,svor=0;
+    //svor = 0;
 	cout << "1 - ojo pasverto pyragėlio svoris, gramais: ";
 	cin >> p1;
 	cout << "2 - ojo pasverto pyragėlio svoris, gramais: ";
@@ -19,42 +20,25 @@ int main ()
 	cin >> p5;
 	cout << "6 - ojo pasverto pyragėlio svoris, gramais: ";
 	cin >> p6;
-	// Papildomas tikrinimas "if..." ir "else if...", jei visų pyragėliu svoris virš 50, mažiau/lygu 50,
-	// tuomet atsakymas bus kitoks.
-	if (p1,p2,p3,p4,p5,p6 < 50) {
-        cout << "Suvalgė visus šešis pyragėlius.";
-	}
-	else if (p1,p2,p3,p4,p5,p6 >= 50) {
-	cout << "Nesuvalgė nė vieno pyragėlio.";
-	}
-	// Jei ne visų svoris <= 50 arba < 50, tada tikrinam po vieną.
-	else {  
-            cout << "Suvalgė ";
-    		is_viso = 0;
-    		if (p1 < 50) {
-                is_viso++;
-                cout << "pirmąjį";
-    		}
-    		if (p2 < 50) {
-                is_viso++;
-                cout << ", antrąjį";
-    		}
-    		if (p3 < 50) {
-                is_viso++;
-                cout << ", trečiąjį";
-    		}
-    		if (p4 < 50) {
-                is_viso++;
-                cout << ", ketvirtąjį";
-            }
-    		if (p5 < 50) {
-                is_viso++;
-                cout << ", penktąjį";
-            }
-            if (p6 < 50) {
-                is_viso++;
-                cout << ", šeštąjį";
-    		}
-            cout << " pyragėlius. Iš viso buvo suvalgyti " << is_viso << " pyragėliai.";
-		}
+
+    svor = 0;
+    if (p1 < 50) {
+        svor = svor + p1;
+    }
+    if (p2 < 50) {
+        svor = svor + p2;
+    }
+    if (p3 < 50) {
+        svor = svor + p3;
+    }
+    if (p4 < 50) {
+        svor = svor + p4;
+    }
+    if (p5 < 50) {
+        svor = svor + p5;
+    }
+    if (p6 < 50) {
+        svor = svor + p6;
+    }
+    cout << "Suvalgytu pyrageliu svoris: " << svor << "g.";
 }
