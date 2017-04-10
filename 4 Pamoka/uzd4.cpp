bbd4f781c154bcf1cbd466e46614a93463e20ac4
UnicodeMan <lveitas956@gmail.com>
2017-04-10 14:59:30 +0300

    Update uzd4.cpp

--- 4 Pamoka/uzd4.cpp
+++ 4 Pamoka/uzd4.cpp
@@ -5,18 +5,26 @@
 // ------------------------------------------------------------------------------------------------------------- //
 // Lygiagrečiai sujungiamų laidininkų varžos apskaičiavimas:                                                     //
 // n = 3, t.y kai laidininkų trys: R1, R2, R3, tai R = 1 / R1 + 1 / R2 + 1 / R3                                  //
-// ------------------------------------------------------------------------------------------------------------- //
-//    **** Užduotis nepradėta! *****                                                                             //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 main()
 
 {
-  setlocale(LC_CTYPE, "");
+    setlocale(LC_CTYPE, "");
+    double n, R=0, Rr, nn=1;
 
+    cout << "Elektros grandinę sudaro lygiagrečiai prijungtų laidininkų: ";
+    cin  >> n;
 
-
+    for (n; n >= nn; nn++) {
+        cout << nn << "-ojo laidininko varža, omais: ";
+        cin  >> Rr;
+        R = R + 1/Rr;
+    }
+    cout << "Elektros grandinės varža: " << fixed << setprecision(2) << R << " omų";
 
 }
+
